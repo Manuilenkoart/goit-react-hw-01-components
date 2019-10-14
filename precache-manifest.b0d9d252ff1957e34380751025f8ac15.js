e663b9f656c06e1c79634bb87d59ef554885983b
Manuilenkoart <manuilenkoart@gmail.com>
2019-10-14 08:13:19 +0300

    Updates

---
+++ precache-manifest.b0d9d252ff1957e34380751025f8ac15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a4dc3b6c6201e3d23ec72893d85a54",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "951d78aad9ab189c446d",
+    "url": "/goit-react-hw-01-components/static/css/main.e26fd3ec.chunk.css"
+  },
+  {
+    "revision": "49215d7d90512ff65a7b",
+    "url": "/goit-react-hw-01-components/static/js/2.ea76af37.chunk.js"
+  },
+  {
+    "revision": "951d78aad9ab189c446d",
+    "url": "/goit-react-hw-01-components/static/js/main.2b2a691b.chunk.js"
+  },
+  {
+    "revision": "4d9ce36fabe79d32c0ed",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.7a5c0bc9.js"
+  }
+]);
